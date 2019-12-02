c4a533beb0222d4e86d964d871ff906e4e435e6a
Haeley Griffin <griffinh@allegheny.edu>
2019-12-02 16:58:55 -0500

    Added decalred varibles

--- BLPF/BLPF/ViewController.swift
+++ BLPF/BLPF/ViewController.swift
@@ -10,6 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var loginScreen: UIView!
+    @IBOutlet weak var search: UISearchBar!
+    @IBOutlet weak var crossfitLogo: UIImageView!
+    @IBOutlet weak var dtLogo: UIImageView!
+    @IBOutlet weak var blpfLogo: UIImageView!
+    @IBOutlet weak var blueBar: UILabel!
+    @IBOutlet weak var homeScreen: UIView!
+    @IBOutlet weak var weighBtn: UIButton!
+    @IBOutlet weak var viewBtn: UIButton!
+    @IBOutlet weak var enterBtn: UIButton!
+    @IBOutlet weak var homeBtn: UIButton!
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var idLbl: UILabel!
+    @IBOutlet weak var bfLbl: UILabel!
+    @IBOutlet weak var tempID: UILabel!
+    @IBOutlet weak var tempName: UILabel!
+    @IBOutlet weak var tempWeight: UILabel!
+    @IBOutlet weak var tempBF: UILabel!
+    @IBOutlet weak var historyScreen: UIView!
+    @IBOutlet weak var weightScreen: UIView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
